d813a3cb0e3f2fe19f9c4a2f5895bc6e9a564012
wangyaohui <wangyaohui@vv.cn>
2020-10-27 19:28:57 +0800

    init

--- src/main/java/com/example/demo/bytecode/ByteCodeStudy.java
+++ src/main/java/com/example/demo/bytecode/ByteCodeStudy.java
@@ -7,6 +7,7 @@ package com.example.demo.bytecode;
  */
 public class ByteCodeStudy {
     public static void main(String[] args) {
+        //查看if和两种for循环的字节码实现
         int[] arr = new int[]{0, 1, 2, 3, 4};
         int a = 0;
         for (int i = 0; i < arr.length; i++) {
